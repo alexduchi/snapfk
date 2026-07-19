473e64a1f45dd9885dff7c4ca7e51c4120064172
alexduchi <nbthr2@outlook.fr>
2026-07-19 08:52:58 +0200

    Fix v0.6 wrapper string escaping

--- holoforge_v06/run_fixed.py
+++ holoforge_v06/run_fixed.py
@@ -5,7 +5,25 @@ script_path = Path(__file__).with_name("prepare_v06.py")
 source = script_path.read_text()
 start = source.index("    marker = '''")
 end = source.index("    layout = replace_once", start)
-replacement = '''    marker = """          <View\n              android:layout_width=\\"38dp\\"""\n    mode_row = """            <LinearLayout\n                android:layout_width=\\"match_parent\\"\n                android:layout_height=\\"42dp\\"\n                android:layout_marginTop=\\"10dp\\"\n                android:orientation=\\"horizontal\\">\n              <TextView\n                  android:id=\\"@+id/mode_touch\\"\n                  style=\\"@style/HoloAxisButton\\"\n                  android:text=\\"TACTILE\\" />\n              <TextView\n                  android:id=\\"@+id/mode_one_hand\\"\n                  style=\\"@style/HoloAxisButton\\"\n                  android:text=\\"UNE MAIN\\" />\n            </LinearLayout>\n\n          <View\n              android:layout_width=\\"38dp\\"""\n'''
+marker = '          <View\n              android:layout_width="38dp"'
+mode_row = '''            <LinearLayout
+                android:layout_width="match_parent"
+                android:layout_height="42dp"
+                android:layout_marginTop="10dp"
+                android:orientation="horizontal">
+              <TextView
+                  android:id="@+id/mode_touch"
+                  style="@style/HoloAxisButton"
+                  android:text="TACTILE" />
+              <TextView
+                  android:id="@+id/mode_one_hand"
+                  style="@style/HoloAxisButton"
+                  android:text="UNE MAIN" />
+            </LinearLayout>
+
+          <View
+              android:layout_width="38dp"'''
+replacement = f"    marker = {marker!r}\n    mode_row = {mode_row!r}\n"
 source = source[:start] + replacement + source[end:]
 namespace = {"__name__": "__main__", "__file__": str(script_path)}
 exec(compile(source, str(script_path), "exec"), namespace)
