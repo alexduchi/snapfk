99f49e08279d559e92db46bc24b92bda88c194e2
alexduchi <nbthr2@outlook.fr>
2026-07-19 08:54:55 +0200

    Make v0.6 mode insertion indentation independent

--- holoforge_v06/run_fixed.py
+++ holoforge_v06/run_fixed.py
@@ -6,24 +6,23 @@ source = script_path.read_text()
 start = source.index("    marker = '''")
 end = source.index("    layout_path.write_text(layout)", start)
 mode_row = '''
-            <LinearLayout
-                android:layout_width="match_parent"
-                android:layout_height="42dp"
-                android:layout_marginTop="10dp"
-                android:orientation="horizontal">
-              <TextView
-                  android:id="@+id/mode_touch"
-                  style="@style/HoloAxisButton"
-                  android:text="TACTILE" />
-              <TextView
-                  android:id="@+id/mode_one_hand"
-                  style="@style/HoloAxisButton"
-                  android:text="UNE MAIN" />
-            </LinearLayout>
-'''
+    <LinearLayout
+        android:layout_width="match_parent"
+        android:layout_height="42dp"
+        android:layout_marginTop="10dp"
+        android:orientation="horizontal">
+      <TextView
+          android:id="@+id/mode_touch"
+          style="@style/HoloAxisButton"
+          android:text="TACTILE" />
+      <TextView
+          android:id="@+id/mode_one_hand"
+          style="@style/HoloAxisButton"
+          android:text="UNE MAIN" />
+    </LinearLayout>'''
 replacement = (
     "    status_pos = layout.index('android:id=\"@+id/axis_status\"')\n"
-    "    insert_pos = layout.index('          </LinearLayout>', status_pos)\n"
+    "    insert_pos = layout.index('/>', status_pos) + 2\n"
     f"    mode_row = {mode_row!r}\n"
     "    layout = layout[:insert_pos] + mode_row + layout[insert_pos:]\n"
 )
