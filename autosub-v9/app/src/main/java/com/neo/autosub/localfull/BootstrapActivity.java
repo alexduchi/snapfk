7e033db5cb84b2daa3bf68d731d13b91ea541260
alexduchi <nbthr2@outlook.fr>
2026-08-16 12:01:06 +0200

    Start AutoSub foreground processing guard

---
+++ autosub-v9/app/src/main/java/com/neo/autosub/localfull/BootstrapActivity.java
@@ -0,0 +1,22 @@
+package com.neo.autosub.localfull;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+
+public class BootstrapActivity extends Activity {
+    @Override protected void onCreate(Bundle state) {
+        super.onCreate(state);
+        Intent service = new Intent(this, ProcessingKeepAliveService.class);
+        service.putExtra(ProcessingKeepAliveService.EXTRA_TEXT,
+                "AutoSub prêt : téléchargement, transcription et export protégés en arrière-plan.");
+        if (Build.VERSION.SDK_INT >= 26) startForegroundService(service);
+        else startService(service);
+
+        Intent main = new Intent(this, MainActivity.class);
+        main.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(main);
+        finish();
+    }
+}
